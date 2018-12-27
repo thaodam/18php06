f627621629dcf7bf288a682da2232ce901e21cca
Dam Huynh Thanh Thao <damthanhthao12@gmail.com>
2018-12-27 19:05:42 +0700

    admin2

--- 18php061/admin/add_product.php
+++ 18php061/admin/add_product.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Boxed Layout</title>
+  <title>AdminLTE 2 | HOME</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -53,13 +53,13 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="dist/img/avatar5.png" class="user-image" alt="User Image">
               <span class="hidden-xs">Thao Dam</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="dist/img/avatar5.png" class="img-circle" alt="User Image">
 
                 <p>
                   Thao Dam - Web Developer
